885240ec1974e4d62861c4606a58b482a6e1518f
IPv6Freely <ipv6freely@gmail.com>
2017-06-28 11:34:00 -0700

    statseeker export completed

--- pwchanger.py
+++ pwchanger.py
@@ -1,1 +1,54 @@
 #!.venv/bin/python
+import requests
+import json
+import credentials
+
+def statseeker_export(username,password):
+    """"""
+    requests.packages.urllib3.disable_warnings()
+    headers = {'Accept':'application/json', 'Content-Type':'application/json'}
+    urlbase = 'https://statseeker.sempra.com/api/latest/cdt_device/'
+    fields  = '?fields=name,.ipaddress,SNMPv2-MIB.sysDescr'
+    filters = '&.snmp_poll_filter=IS(\u0027on\u0027)&.ping_poll_filter=IS(\u0027on\u0027)'
+    links   = '&links=none'
+    limit   = '&limit=0'
+    groups  = '&groups=23,26,161562,161563'
+    url = urlbase + fields + filters + links + limit
+
+    devices = requests.get(url, headers=headers, auth=(username, password), verify=False).json()
+
+    print(f"\n{'HOSTNAME':40} {'IP ADDRESS':20} {'VENDOR':30}")
+    print("="*90)
+
+    device_types = ['Juniper', 'Cisco', 'Brocade', 'Foundry']
+    device_list = []
+
+    for device in devices['data']['objects'][0]['data']:
+        if device['SNMPv2-MIB.sysDescr']:
+            if any(x in device['SNMPv2-MIB.sysDescr'] for x in device_types):
+                tempdict = {}
+                tempdict['name'] = device['name']
+                tempdict['ipaddress'] = device['.ipaddress']
+                tempdict['vendor'] = device['SNMPv2-MIB.sysDescr'].split()[0]
+                device_list.append(tempdict)
+
+    return device_list
+        
+def main():
+    """"""
+    requests.packages.urllib3.disable_warnings()
+
+    device_list = statseeker_export(credentials.statseeker_username,credentials.statseeker_password)
+
+    for device in device_list:
+
+        print(f"{device['name']:40} {device['ipaddress']:20} {device['vendor']:30}")
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n\nCTRL+C Pressed. Exiting.\n\n")
+        pass
+exit()
